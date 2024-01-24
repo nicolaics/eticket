59e765c5dfed8e850e00b9f47244c88a594e07b5
nicolaics <nicolai.christian.s@gmail.com>
2024-01-25 00:25:15 +0700

    init

---
+++ choices.py
@@ -0,0 +1,24 @@
+use_choice = [
+    "Batuk",
+    "Pilek",
+    "Batuk dan Pilek",
+    "Batuk Sesak",
+    "Maag",
+    "Sakit / Nyeri",
+    "Antibiotik / Radang Tenggorokan",
+    "Vitamin",
+    "Obat Tidur / Penenang",
+    "Yang lain:"
+]
+
+unit_choice = [
+    "Tablet",
+    "Kapsul",
+    "Bungkus"
+]
+
+consume_time_choice = [
+    "Sebelum Makan",
+    "Sesudah Makan",
+    "Saat Makan"
+]
