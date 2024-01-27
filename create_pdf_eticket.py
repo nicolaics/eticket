b812ad931d8eff2f74eadbd3673e548dd340f51c
nicolaics <nicolai.christian.s@gmail.com>
2024-01-27 18:48:42 +0700

    7x5

--- create_pdf_eticket.py
+++ create_pdf_eticket.py
@@ -1,19 +1,21 @@
 from fpdf import FPDF
 from fpdf.enums import XPos, YPos
 from datetime import date
+
 import os
 
-def get_num_of_lines_in_multicell(pdf, message, CELL_WIDTH):
+def get_num_of_lines_in_multicell(pdf, message, width):
     # divide the string in words
     words = message.split(" ")
     line = ""
     n = 1
+
     for word in words:
         line += word + " "
         line_width = pdf.get_string_width(line)
 
         # In the next if it is necessary subtract 1 to the WIDTH
-        if line_width > CELL_WIDTH - 1:
+        if line_width > width - 1.1:
             # the multi_cell() insert a line break
             n += 1
             # reset of the string
@@ -29,7 +31,7 @@ def create_pdf_eticket(data):
     FONT_SIZE = 10
     BIGGER_FONT_SIZE = 12
     CELL_HEIGHT = 0.7
-    MARGIN = 0.1
+    MARGIN = 0.15
     MAX_WIDTH = PAPER_WIDTH - (MARGIN * 2)
 
     today = date.strftime(date.today(), "%d-%m-%Y")
@@ -72,7 +74,7 @@ def create_pdf_eticket(data):
     pdf.set_font(FONT_NAME, BOLD, FONT_SIZE)
     pdf.cell((TOP_COLUMN_WIDTH - num_col_width), CELL_HEIGHT, data["num"])
 
-    pdf.set_x(2.3)
+    pdf.set_x(2.1)
 
     pdf.set_font(FONT_NAME, REGULAR, FONT_SIZE)
     title_width = pdf.get_string_width("Tgl:")
@@ -93,14 +95,17 @@ def create_pdf_eticket(data):
     # Set the Y-axis for the name title to be in the middle of the box
     pdf.y += 0.6
 
-    title_width = pdf.get_string_width("Nama:")
-    name_col_width = title_width
+    name_col_width = pdf.get_string_width("Nama:")
 
     pdf.set_font(FONT_NAME, REGULAR, FONT_SIZE)
     pdf.cell(name_col_width, None, "Nama:")
 
-    num_of_lines = get_num_of_lines_in_multicell(pdf, data["name"], (MAX_WIDTH - name_col_width))
-    # print(num_of_lines_name)
+    pdf.set_font(FONT_NAME, BOLD, BIGGER_FONT_SIZE)
+
+    name_size_limit = (MAX_WIDTH - name_col_width)
+
+    num_of_lines = get_num_of_lines_in_multicell(pdf, data["name"], name_size_limit)
+    # print(num_of_lines)
 
     # To reset the Y-axis for the name from the title
     pdf.y = temp_y
@@ -110,7 +115,6 @@ def create_pdf_eticket(data):
 
     pdf.x = name_col_width + MARGIN
 
-    pdf.set_font(FONT_NAME, BOLD, BIGGER_FONT_SIZE)
     pdf.multi_cell((MAX_WIDTH - name_col_width - (MARGIN * 2)), CELL_HEIGHT, data["name"], align='C', new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 
     print("After Name: {0}, {1}".format(pdf.x, pdf.y))
@@ -124,13 +128,15 @@ def create_pdf_eticket(data):
     temp_y = pdf.y
     third_line_y = temp_y + (CELL_HEIGHT * 2)
 
+    pdf.set_font(FONT_NAME, REGULAR, BIGGER_FONT_SIZE)
+
     num_of_lines = get_num_of_lines_in_multicell(pdf, data["use"], MAX_WIDTH)
+    # print(num_of_lines)
 
     # To put the use in the middle of the box
     if num_of_lines == 1:
         pdf.y += 0.45
 
-    pdf.set_font(FONT_NAME, REGULAR, BIGGER_FONT_SIZE)
     pdf.multi_cell(MAX_WIDTH, CELL_HEIGHT, data["use"], align='C', new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 
     print("After use: {0}, {1:.1f}".format(pdf.x, pdf.y))
@@ -151,13 +157,11 @@ def create_pdf_eticket(data):
 
     dose_pos_y = third_line_y + 0.15
 
-    pdf.line(2.5, 0, 2.5, PAPER_HEIGHT)
-
     pdf.set_font(FONT_NAME, BOLD, BIGGER_FONT_SIZE)
     pdf.set_xy(1.95, dose_pos_y)
     pdf.write_html(data["dose"])
 
-    pdf.set_xy(3.2, third_line_y)
+    pdf.set_xy(3.3, third_line_y)
     pdf.set_font(FONT_NAME, REGULAR, FONT_SIZE)
     pdf.cell(unit_width, CELL_HEIGHT, data["unit"], new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 
@@ -197,7 +201,7 @@ def create_pdf_eticket(data):
 if __name__ == "__main__":
     data = {
         "num": "123",
-        "name": "Nicolai Christian Suhalim",
+        "name": "Nicolai Christian",
         "use": "Antibiotik / Radang Tenggorokan",
         # "use": "Antibiotik",
         "dose": "3 x 1",
--- print_using_acrobat.py
+++ print_using_acrobat.py
@@ -1,48 +1,54 @@
 import win32print
 import win32api
+import win32con
 from tkinter import messagebox
 
 def print_using_acrobat(file_name, printer_name):
-    DM_OUT_BUFFER = 0x02
-    DM_IN_BUFFER = 0x08
-
     PRINTER_DEFAULTS = {"DesiredAccess":win32print.PRINTER_ALL_ACCESS}
     printer_handler = win32print.OpenPrinter(printer_name, PRINTER_DEFAULTS)
     level = 2
     properties = win32print.GetPrinter(printer_handler, level)
     pDevModeObj = properties["pDevMode"]
-    
-    pDevModeObj.PaperLength = 6000 #SIZE IN 1/10 mm
-    pDevModeObj.PaperWidth = 7000 #SIZE IN 1/10 mm
-    pDevModeObj.Color = 2
-    pDevModeObj.PrintQuality = 100
+
+    pDevModeObj.Orientation = win32con.DMORIENT_LANDSCAPE # Change into landscape
+    pDevModeObj.PaperLength = 500 #SIZE IN 1/10 mm
+    pDevModeObj.PaperWidth = 740 #SIZE IN 1/10 mm
 
     properties["pDevMode"] = pDevModeObj
 
     print(pDevModeObj.PrintQuality)
     print(pDevModeObj.Color)
 
-    win32print.DocumentProperties(None, printer_handler, printer_name, pDevModeObj, pDevModeObj, DM_IN_BUFFER | DM_OUT_BUFFER)
+    # win32print.DocumentProperties(None, printer_handler, printer_name, pDevModeObj, pDevModeObj, win32con.DM_IN_PROMPT | win32con.DM_IN_BUFFER | win32con.DM_OUT_BUFFER)
+    win32print.DocumentProperties(None, printer_handler, printer_name, pDevModeObj, pDevModeObj, win32con.DM_IN_BUFFER | win32con.DM_OUT_BUFFER)
 
-    win32api.ShellExecute(
-            0,
-            "printto",
-            file_name,
-            '"%s"' % printer_name,
-            ".",
-            0
-        )
+    # win32api.ShellExecute(
+    #         0,
+    #         "printto",
+    #         file_name,
+    #         '"%s"' % printer_name,
+    #         ".",
+    #         0
+    #     )
     
     win32print.ClosePrinter(printer_handler)
 
-    messagebox.showinfo("Printing", "Now Printing...")
+    # messagebox.showinfo("Printing", "Now Printing...")
 
 if __name__ == "__main__":
     # file_path = r"D:/etiket_25-01-2024/123_Christian suhalim_25-01-2024.pdf"
     file_path = "trial_7x5.pdf"
     
     printers = win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL, None, 1)
-    printer_name = printers[5][2]
+
+    count = 1
+    for it in printers:
+        print(count, ". ", it[2])
+        count += 1
+
+    # select = int(input("Select: ")) - 1
+    select = 0
+    printer_name = printers[select][2]
 
 
     print_using_acrobat(file_path, printer_name)
